8ecaacd05231d914a09277af8c1fc6b69cd262c4
Cesar Landaverde <20230355@ricaldone.edu.sv>
2025-06-15 19:03:49 -0600

    se creo app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,1 +1,14 @@
-ksjabckjsac
+import express from "express";
+import cors from "cors";
+import productRoutes from "./routes/products.js";
+import dotenv from "dotenv";
+import cookiparser from "cookie-parser";
+
+
+dotenv.config();
+
+const app = exxpress();
+
+const allowedOrigins = [
+    "*"
+]
