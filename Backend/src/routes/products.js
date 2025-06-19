8b838d68c7f9865970ebd7e6233d26dc42c2ca83
Cesar Landaverde <20230355@ricaldone.edu.sv>
2025-06-18 23:52:53 -0600

    ya funciona el insert

--- Backend/src/routes/products.js
+++ Backend/src/routes/products.js
@@ -1,18 +1,21 @@
 import express from 'express';
 const router = express.Router();
 import productsController from '../controllers/productsController.js';
-import { uploadFlexible } from '../config/multer.js';
+import multer from "multer";
+
+const upload = multer({dest:"public/"})
 
 
 //Nos aseguramos que exista la carpeta de subida 
 router.route("/")
-.get(productsController.getProducts)
-.post(uploadFlexible, productsController.createProducts);
+router.route("/")
+  .get(productsController.getAllProducts)
+  .post(upload.array("images"), productsController.insertProduct);
 
 router.route("/:id")
-.get(productsController.getProductById)
-.put(uploadFlexible, productsController.updateProduct)
-.delete(productsController.deleteProduct);
+  .get(productsController.getProductById)
+  .put(upload.array("images"), productsController.updateProduct)
+  .delete(productsController.deleteProduct);
 
 export default router;
 
