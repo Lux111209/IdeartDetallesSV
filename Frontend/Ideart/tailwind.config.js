11b6c0d2f0fb3ba6812fac7e537f44db2a7f93dd
Cesar Landaverde <20230355@ricaldone.edu.sv>
2025-07-15 01:27:23 -0600

    configure tailwind para que no afecte todo , manana acabo las 3 3 pantallas

---
+++ Frontend/Ideart/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'tw-', // Se  Agrega esto para evitar conflictos globales
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
