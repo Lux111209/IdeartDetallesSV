5328e505ff67ea21e1907a79def674c4253b0373
Lux111209 <lmgaspariomendez@gmail.com>
2025-10-16 09:14:03 -0600

    Import

--- Backend/src/routes/paymentRecordRoutes.js
+++ Backend/src/routes/paymentRecordRoutes.js
@@ -1,6 +1,6 @@
-const express = require("express");
+import express from "express";
 const router = express.Router();
-const controller = require("../controllers/paymentRecordsController");
+import controller from "../controllers/paymentRecordsController";
 
 router.post("/", controller.createPaymentRecord);
 router.get("/", controller.getAllPaymentRecords);
